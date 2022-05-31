d00842188b8bacca9eb887fb383a2012298bf4eb
FlutLab.io <>
2022-05-31 17:30:03 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/Villegas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.Villegas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:Villegas/models/catalog.dart';
+import 'package:Villegas/widgets/itemWidget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Catalogo"),
+        ),
+        body: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: CatalogModel.items[index]);
+            }),
+      ),
+    );
+  }
+}
